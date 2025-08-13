c0c997564aed6ce8d5f693de4862c8f5775af1d2
chathuraIndunil <chathuraindunil807@gmail.com>
2025-08-14 03:03:26 +0530

    addCssToForm

--- src/UsersForm.js
+++ src/UsersForm.js
@@ -1,5 +1,53 @@
-const UsersForm = props =>{
+import { Grid, Input, Typography } from "@mui/material";
 
+const UserForm = props => {
+    return (
+        <Grid
+            container
+            spacing={2}
+            sx={{
+                backgroundColor:'#fffff',
+                marginBottom: '30px',
+                display:'block',
+            }
 
+            }
+        >
+            <Grid item xs ={12}>
+                <Typography component={'h1'} sx={{color:'#00000'}}> User Form</Typography>
+
+            </Grid>
+
+            <Grid item xs={12}  sm={6} sx={{ display:'flex'}}>
+                <Typography component={('label') htmlFor="id"}
+                sx={{
+                    color:'#000000',
+                    marginRight:'20px',
+                    fontSize:'16px',
+                    width:'100px',
+                    display:'block',
+                }}>
+                 ID
+                </Typography>
+                <Input 
+                    type="number"
+                    id='id'
+                    name="id"
+                    sx={{ width:'400px'}}
+                    value={''}
+                    onChange={e => {}}
+                
+                
+                />
+                
+
+            </Grid>
+
+            
+
+        </Grid>
+
+    );
 }
-export default UsersForm;
+
+export default UserForm ;
