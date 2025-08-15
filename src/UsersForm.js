61c2030b05ced900a94bca8e06776bbba755298a
chathuraIndunil <chathuraindunil807@gmail.com>
2025-08-15 23:51:42 +0530

    updateuserform

--- src/UsersForm.js
+++ src/UsersForm.js
@@ -1,53 +1,106 @@
-import { Grid, Input, Typography } from "@mui/material";
+import { Grid, Input, Typography, Button } from "@mui/material";
+import { useState } from "react";
 
-const UserForm = props => {
-    return (
-        <Grid
-            container
-            spacing={2}
-            sx={{
-                backgroundColor:'#fffff',
-                marginBottom: '30px',
-                display:'block',
-            }
+const UserForm = () => {
+  const [form, setForm] = useState({ id: "", name: "" });
 
-            }
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = () => {
+    console.log("Form Submitted:", form);
+  };
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        backgroundColor: "#ffffff",
+        marginBottom: "30px",
+        display: "block",
+        padding: "20px",
+      }}
+    >
+      <Grid item xs={12}>
+        <Typography
+          component="h1"
+          sx={{ color: "#000000", fontSize: "24px", marginBottom: "20px" }}
         >
-            <Grid item xs ={12}>
-                <Typography component={'h1'} sx={{color:'#00000'}}> User Form</Typography>
+          User Form
+        </Typography>
+      </Grid>
 
-            </Grid>
+      {/* ID */}
+      <Grid item xs={12} sm={6} sx={{ display: "flex", alignItems: "center" }}>
+        <Typography
+          component="label"
+          htmlFor="id"
+          sx={{
+            color: "#000000",
+            marginRight: "20px",
+            fontSize: "16px",
+            width: "100px",
+            display: "block",
+          }}
+        >
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id="id"
+          name="id"
+          sx={{ width: "400px" }}
+          value={form.id}
+          onChange={handleChange}
+        />
+      </Grid>
 
-            <Grid item xs={12}  sm={6} sx={{ display:'flex'}}>
-                <Typography component={('label') htmlFor="id"}
-                sx={{
-                    color:'#000000',
-                    marginRight:'20px',
-                    fontSize:'16px',
-                    width:'100px',
-                    display:'block',
-                }}>
-                 ID
-                </Typography>
-                <Input 
-                    type="number"
-                    id='id'
-                    name="id"
-                    sx={{ width:'400px'}}
-                    value={''}
-                    onChange={e => {}}
-                
-                
-                />
-                
+      {/* Name */}
+      <Grid item xs={12} sm={6} sx={{ display: "flex", alignItems: "center" }}>
+        <Typography
+          component="label"
+          htmlFor="name"
+          sx={{
+            color: "#000000",
+            marginRight: "20px",
+            fontSize: "16px",
+            width: "100px",
+            display: "block",
+          }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          sx={{ width: "400px" }}
+          value={form.name}
+          onChange={handleChange}
+        />
+      </Grid>
 
-            </Grid>
+      {/* Submit */}
+      <Grid item xs={12}>
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          sx={{
+            marginTop: "20px",
+            backgroundColor: "#00c6e6",
+            "&:hover": {
+              opacity: 0.7,
+              backgroundColor: "#00c6e6",
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
 
-            
-
-        </Grid>
-
-    );
-}
-
-export default UserForm ;
+export default UserForm;
